8e90b4a4fd94f31662d040a01118a05435cf8081
root <root@haoyu_yang.localdomain>
2024-04-04 11:16:16 +0200

    rm unrelated files in norm2/cuda

--- norm2/cuda/vector_imp.cpp
+++
@@ -1,7 +0,0 @@
-#include "vector_imp.hpp"
-
-void axpy(const size_t n, const float alpha, const float* x, const float* y, float* z) {
-    for (size_t i = 0; i < n; ++i) {
-        z[i] = alpha * x[i] + y[i];
-    }
-}
--- norm2/cuda/vector_imp.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef VECTOR_IMP_HPP
-#define VECTOR_IMP_HPP
-
-#include <cstddef>
-
-void axpy(const size_t n, const float alpha, const float* x, const float* y, float* z);
-
-#endif 
